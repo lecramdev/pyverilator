64b8294ff1afebb47be76fcad6ae87027e0402c2
Yaman Umuroglu <yamanu@xilinx.com>
2022-06-02 09:50:31 +0200

    remove pyverilate_stitched_ip as this is too FINN-specific

--- pyverilator/util/axi_utils.py
+++ pyverilator/util/axi_utils.py
@@ -29,8 +29,6 @@
 
 import pkg_resources as pk
 
-import os
-
 from pyverilator import PyVerilator
 
 axilite_expected_signals = [
@@ -207,89 +205,6 @@ def rtlsim_multi_io(
         sim.stop_vcd_trace()
 
     return total_cycle_count
-
-
-def pyverilate_stitched_ip(
-    all_verilog_srcs,
-    build_dir,
-    top_module_file_name,
-    read_internal_signals=True,
-    disable_common_warnings=True,
-    extra_verilator_args=[],
-    trace_depth=3,
-):
-    """Given a model with stitched IP, return a PyVerilator sim object.
-    Trace depth is also controllable, see get_rtlsim_trace_depth()
-
-    :param read_internal_signals  If set, it will be possible to examine the
-        internal (not only port) signals of the Verilog module, but this may
-        slow down compilation and emulation.
-
-    :param disable_common_warnings If set, disable the set of warnings that
-        Vivado-HLS-generated Verilog typically triggers in Verilator
-        (which can be very verbose otherwise)
-
-    """
-
-    def file_to_dir(x):
-        return os.path.dirname(os.path.realpath(x))
-
-    def file_to_basename(x):
-        return os.path.basename(os.path.realpath(x))
-
-    top_module_name = top_module_file_name.strip(".v")
-
-    # dump all Verilog code to a single file
-    # this is because large models with many files require
-    # a verilator command line too long for bash on most systems
-    # NOTE: there are duplicates in this list, and some files
-    # are identical but in multiple directories (regslice_core.v)
-
-    # remove duplicates from list by doing list -> set -> list
-    all_verilog_files = list(set(filter(lambda x: x.endswith(".v"), all_verilog_srcs)))
-
-    # remove all but one instances of regslice_core.v
-    filtered_verilog_files = []
-    remove_entry = False
-    for vfile in all_verilog_files:
-        if "regslice_core" in vfile:
-            if not remove_entry:
-                filtered_verilog_files.append(vfile)
-            remove_entry = True
-        else:
-            filtered_verilog_files.append(vfile)
-
-    # concatenate all verilog code into a single file
-    with open(build_dir + "/" + top_module_file_name, "w") as wf:
-        for vfile in filtered_verilog_files:
-            with open(vfile) as rf:
-                wf.write("//Added from " + vfile + "\n\n")
-                wf.write(rf.read())
-
-    verilator_args = []
-    # disable common verilator warnings that should be harmless but commonly occur
-    # in large quantities for Vivado HLS-generated verilog code
-    if disable_common_warnings:
-        verilator_args += ["-Wno-STMTDLY"]
-        verilator_args += ["-Wno-PINMISSING"]
-        verilator_args += ["-Wno-IMPLICIT"]
-        verilator_args += ["-Wno-WIDTH"]
-        verilator_args += ["-Wno-COMBDLY"]
-    # force inlining of all submodules to ensure we can read internal signals properly
-    if read_internal_signals:
-        verilator_args += ["--inline-mult", "0"]
-
-    sim = PyVerilator.build(
-        top_module_file_name,
-        verilog_path=[build_dir],
-        build_dir=build_dir,
-        trace_depth=trace_depth,
-        top_module_name=top_module_name,
-        auto_eval=False,
-        read_internal_signals=read_internal_signals,
-        extra_args=verilator_args + extra_verilator_args,
-    )
-    return sim
 
 
 def _find_signal(sim, signal_name):
